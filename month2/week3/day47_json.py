0d4ee816414dcac66f9d2e8999896034c6a440f1
Kartik Budhlakoti <professorkarma17@gmail.com>
2026-04-18 16:43:38 +0530

    writing json in different ways

--- month2/week3/day47_json.py
+++ month2/week3/day47_json.py
@@ -1,8 +1,8 @@
 import json
 from pathlib import Path
 
-# # Python to json string
-# print(f"=== Python to json ===")
+# Python to json string
+print(f"=== Python to json ===")
 
 data = {
     'name':'kartik',
@@ -21,9 +21,9 @@ json_string= json.dumps(data,indent=4)
 print(json_string)
 print(f"\nType: {type(json_string)}")
 
-# # without indent == not human readable
-# compact = json.dumps(data)
-# print(f"\nCompact: {compact}")
+# without indent == not human readable
+compact = json.dumps(data)
+print(f"\nCompact: {compact}")
 
 # Json to Python
 print(f"=== Json to Python ===")
@@ -40,28 +40,28 @@ print(f"Python score : {parsed['scores']['python']}")
 # print("=== Writing Json to File ===")
 output_path = Path('/home/kartik007/Desktop/python-new/python-journey/month2/week3/data.json')
 
-# # Method 1- using json.dumps + write_text
-# output_path.write_text(json.dumps(data , indent=4))
-# print(f"Written to : {output_path}")
+# Method 1- using json.dumps + write_text
+output_path.write_text(json.dumps(data , indent=4))
+print(f"Written to : {output_path}")
 
-# # Method 2- using json.dump with open()
-# with open(output_path , 'w') as f:
-#     json.dump(data ,f, indent=4)
-# print(f"Written again with json.dump")
+# Method 2- using json.dump with open()
+with open(output_path , 'w') as f:
+    json.dump(data ,f, indent=4)
+print(f"Written again with json.dump")
 
 
-# # Reading json file with python
-# print(f"\n === Reading Json from Python === ")
+# Reading json file with python
+print(f"\n === Reading Json from Python === ")
 
-# # Method 1- using read_text + json_loads
-# content = output_path.read_text()
-# loaded = json.loads(content)
-# print(f"Loaded name: {loaded['name']}")
+# Method 1- using read_text + json_loads
+content = output_path.read_text()
+loaded = json.loads(content)
+print(f"Loaded name: {loaded['name']}")
 
-# # Method 2- using json.load with open()
-# with open(output_path, 'r') as f: 
-#     loaded2 = json.load(f)
-# print(f"Loaded skills : {loaded2['skills']}")
+# Method 2- using json.load with open()
+with open(output_path, 'r') as f: 
+    loaded2 = json.load(f)
+print(f"Loaded skills : {loaded2['skills']}")
 
 # Navigation of Nested Json 
 print(f"\n === Navigating NESTED JSON === ")
